1f23677acc3b01e8c06a721fbf5f199e26e0a338
beyzaaydogan01 <113782704+beyzaaydogan01@users.noreply.github.com>
2024-12-28 18:14:19 +0300

    context and migration added

---
+++ src/projects/ECommerce.Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace ECommerce.Persistence.Contexts;
+
+public class BaseDbContext : DbContext
+{
+    public BaseDbContext(DbContextOptions<BaseDbContext> opt): base(opt)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+    DbSet<Category> Categories { get; set; }
+}
