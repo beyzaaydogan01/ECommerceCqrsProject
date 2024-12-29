3f6431887969fd2ed22b82fde6d83f527e720353
beyzaaydogan01 <113782704+beyzaaydogan01@users.noreply.github.com>
2024-12-29 13:06:02 +0300

    rule, validator and constants added

---
+++ src/projects/ECommerce.Application/Features/Categories/Constants/CategoryMessages.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ECommerce.Application.Features.Categories.Constants;
+
+public static class CategoryMessages
+{
+    public const string CategoryNotFoundMessage = "Category Not Found.";
+    public const string CategoryNameMustBeUniqueMessage = "Category Name Must Be Unique!";
+    public const string CategoryNameMustBeMinimumThreeLength = "Category name must be contains min 3 character.";
+}
