2c1812bee3adc261cec1ff0e8764c82918e7a161
beyzaaydogan01 <113782704+beyzaaydogan01@users.noreply.github.com>
2025-01-05 21:27:30 +0300

    constants, rules and dependencies added

--- src/projects/ECommerce.Application/ApplicationServiceRegistration.cs
+++ src/projects/ECommerce.Application/ApplicationServiceRegistration.cs
@@ -6,7 +6,11 @@ using Core.Application.Pipelines.Performance;
 using Core.Application.Pipelines.Transaction;
 using Core.Application.Pipelines.Validation;
 using Core.CrossCuttingConcerns.Serilog.Loggers;
+using ECommerce.Application.Features.Auth.Rules;
 using ECommerce.Application.Features.Categories.Rules;
+using ECommerce.Application.Services.RoleServices;
+using ECommerce.Application.Services.UserServices;
+using ECommerce.Application.Services.UserWithTokenServices;
 using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,8 +22,11 @@ public static class ApplicationServiceRegistration
 {
     public static IServiceCollection AddApplicationServiceDependencies(this IServiceCollection services)
     {
+        services.AddScoped<UserBusinessRules>();
         services.AddScoped<CategoryBusinessRules>();
-
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IUserWithTokenService, UserWithTokenService>();
+        services.AddScoped<IRoleService, RoleService>();
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehavior<,>));
         services.AddValidatorsFromAssemblies([Assembly.GetExecutingAssembly()]);
         services.AddAutoMapper(Assembly.GetExecutingAssembly()); 
