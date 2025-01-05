f17785f221f813f384b463ae526714c56566d450
beyzaaydogan01 <113782704+beyzaaydogan01@users.noreply.github.com>
2025-01-05 21:26:13 +0300

    role service added

---
+++ src/projects/ECommerce.Application/Services/RoleServices/IRoleService.cs
@@ -0,0 +1,9 @@
+﻿using ECommerce.Application.Services.UserServices;
+using ECommerce.Domain.Entities;
+
+namespace ECommerce.Application.Services.RoleServices;
+
+public interface IRoleService
+{
+    Task AddRoleToUserAsync(AppUser user, string roleName);
+}
