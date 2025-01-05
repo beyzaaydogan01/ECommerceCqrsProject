0ca01a7bd0d4cacb1b4f072a27996f85f900f58a
beyzaaydogan01 <113782704+beyzaaydogan01@users.noreply.github.com>
2025-01-05 21:25:12 +0300

    User login and register added

--- src/projects/ECommerce.WebAPI/Controllers/AuthController.cs
+++ src/projects/ECommerce.WebAPI/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Security.Dtos;
 using ECommerce.Application.Features.Auth.Commands.AuthLogin;
+using ECommerce.Application.Features.Auth.Commands.AuthRegister;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +11,18 @@ namespace ECommerce.WebAPI.Controllers
     [ApiController]
     public class AuthController : BaseController
     {
+
         [HttpPost("login")]
         public async Task<IActionResult> LoginForUser(UserForLoginDto dto)
         {
             var response = await Mediator.Send(new Login.Command(dto));
+            return Ok(response);
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterForUser(UserForRegisterDto dto) 
+        {
+            var response = await Mediator.Send(new Register.Command(dto));
             return Ok(response);
         }
     }
